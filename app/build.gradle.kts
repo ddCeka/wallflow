a9ad8cb148a289aed5c37b24b42a6e9c2e7db0d9
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-07-27 04:40:58 +0700

    release v2.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.ammar.wallflow"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "2.6.3"
+        versionCode = 26
+        versionName = "2.6.4"
 
         val abi = getAbi()
         ndk {
