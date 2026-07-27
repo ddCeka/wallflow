3cfc313657253d3ed02857d457273d98bc6b346c
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-07-27 08:14:05 +0700

    use jdk 17

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -14,12 +14,12 @@ android {
     compileSdk = 35
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlin {
-        jvmToolchain(11)
+        jvmToolchain(17)
     }
 
     defaultConfig {
